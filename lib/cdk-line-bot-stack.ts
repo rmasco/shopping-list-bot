243d31884df02972e354d32c14ee96bc4eade84f
Ryota Masuko <r.g.masuko@gmail.com>
2020-07-02 12:37:13 +0000

    test

--- lib/cdk-line-bot-stack.ts
+++ lib/cdk-line-bot-stack.ts
@@ -7,7 +7,43 @@ export class CdkLineBotStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    // Implement cdk
+    const table = new dynamodb.Table(this, 'Table', {
+      partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING }
+    })
+
+    const layer = new lambda.LayerVersion(this, 'layer', {
+      compatibleRuntimes: [lambda.Runtime.NODEJS_12_X],
+      code: lambda.Code.fromAsset('layer.out'),
+    })
+
+    const dbHandler = new lambda.Function(this, 'DbHandlerFunction', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda/dbHandler'),
+      layers: [layer],
+      environment: {
+        TABLE_NAME: table.tableName,
+      }
+    })
+
+    const linebot = new lambda.Function(this, 'LineBotFunction', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda/linebot'),
+      layers: [layer],
+      environment: {
+        ACCESS_TOKEN: process.env.ACCESS_TOKEN!,
+        CHANNEL_SECRET: process.env.CHANNEL_SECRET!,
+        FUNCTION_NAME: dbHandler.functionName,
+      }
+    })
+
+    dbHandler.grantInvoke(linebot) // Lambdaの呼び出し権限付与
+
+    table.grantFullAccess(dbHandler)  // DynamoDBの操作権限付与
+
+    const api = new apigateway.RestApi(this, 'Api')
+    api.root.addMethod('POST', new apigateway.LambdaIntegration(linebot))
 
   }
 }
